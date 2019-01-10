c73969e11d7dd89fd3f48d8fc30fea450bf545ed
Daniel Vestøl <tranen.bananen@gmail.com>
2019-01-10 17:57:24 +0100

    Moved database into seperate file, added multiuser authentication system

--- index.js
+++ index.js
@@ -7,56 +7,20 @@ var app = express()
 app.use(bodyParser.json())
 app.use(express.static('www'))
 
-let db = {
-    rooms: [{
-        name: "Anode",
-        roomNumber: 136,
-        desc: "",
-        booking: [],
-    },{
-        name: "Katode",
-        roomNumber: 139,
-        desc: "",
-        booking: [],
-    },{
-        name: "Coloumb",
-        roomNumber: 148,
-        desc: "",
-        booking: [],
-    },{
-        name: "AC",
-        roomNumber: 119,
-        desc: "",
-        booking: [],
-    },{
-        name: "DC",
-        roomNumber: 118,
-        desc: "",
-        booking: [],
-    },{
-        name: "RAM",
-        roomNumber: 110,
-        desc: "Inne på VG1",
-        booking: [],
-    },{
-        name: "Rho",
-        roomNumber: 144,
-        desc: "Lærerenes rom",
-        booking: [],
-    },],
-};
-try {
-    db = JSON.parse(fs.readFileSync("db.json"));
-} catch (e) { }
+let db = JSON.parse(fs.readFileSync("db.json"));
 
 app.get('/api/rooms', function (req, res) {
     res.send(db.rooms);
 });
 app.post("/api/booking", (req, res) => {
-    if (req.body && req.body.name &&
-        req.body.pass &&
-        Number(req.body.pass) == 123 &&
-        req.body.room && req.body.from && req.body.to && moment(req.body.from).isValid() && moment(req.body.to).isValid()) {
+    // validate input (otherwise reject request)
+    if (req.body && req.body.name
+        && authenticated(req.body.pass)
+        && req.body.room
+        && req.body.from
+        && req.body.to
+        && moment(req.body.from).isValid() && moment(req.body.to).isValid()) {
+        // try adding reservation
         let rom = db.rooms.filter(x => x.name == req.body.room)[0];
         if (rom) {
             let displayTime = moment(req.body.from).format('DD/MM, hh:mm')
@@ -76,15 +40,32 @@ app.post("/api/booking", (req, res) => {
         } else {
             res.send({
                 ok: false,
-                msg: "Did not find room;"
+                msg: "Did not find room"
             })
         }
     } else {
         res.send({
             ok: false,
             msg: "Auth/verification failed"
-        });
+        })
     }
 })
 
+function authenticated(PIN = "nope") {
+    if (isNaN(Number(PIN))) {
+        return 0;
+    } else {
+        var PIN = Number(PIN)
+    }
+    let users = userdb.users.filter(user => user.PIN = PIN);
+    if (users.length > 1) {
+        throw new Error("Yikes, there should never be duplicate PINs! (see PIN " + PIN + ")")
+    } else if (users.length < 1) {
+        return 0;
+    } else if (users[0].admin) {
+        return 2;
+    } else {
+        return 1;
+    }
+}
 app.listen(3000)
